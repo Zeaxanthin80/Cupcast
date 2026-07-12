5eb4085c6272db0b9b05d153fb74ffcbb47a8396
Jose Marquez <jose.marquez012@mymdc.net>
2026-07-12 14:43:37 -0400

    Initial Commit

---
+++ FIFA2026WorldCup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FIFA2026WorldCup
+//
+//  Created by user on 7/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FIFA2026WorldCup/FIFA2026WorldCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIFA2026WorldCupApp.swift
+//  FIFA2026WorldCup
+//
+//  Created by user on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct FIFA2026WorldCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
